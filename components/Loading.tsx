cf2015a14b7f137cf9b7061652bd029c97ff0c6b
Najib Hossain <najibhossn@gmail.com>
2025-08-18 11:27:37 +0600

    Add suspense fallback component

---
+++ components/Loading.tsx
@@ -0,0 +1,13 @@
+export default function Loading() {
+  return (
+    <div className="h-auto w-full py-12 flex justify-center items-center">
+      <div className="h-auto w-full text-center">
+        <h2 className="text-lg font-text font-medium
+          text-zinc-900 dark:text-white"
+        >
+          Loading
+        </h2>
+      </div>
+    </div>
+  )
+}
