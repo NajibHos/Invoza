ddef13daf01dce47cf11595328f302428cc3e9df
Najib Hossain <najibhossn@gmail.com>
2025-08-27 15:23:53 +0600

    Add cards skeleton for streaming fallback ui

---
+++ components/Cards-skeleton.tsx
@@ -0,0 +1,9 @@
+import { Skeleton } from "./ui/skeleton";
+
+export default function CardsSkeleton() {
+    return (
+        <>
+        <Skeleton className="h-auto w-full py-24" />
+        </>
+    )
+}
